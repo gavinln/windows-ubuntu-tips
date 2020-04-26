b5a7f9587029d0c1ae29db3b1158c73020e9a4d5
Gavin Noronha <gavinln@hotmail.com>
2020-04-25 23:30:28 -0700

    add rclone to setup

--- default.nix
+++ default.nix
@@ -14,43 +14,43 @@ let
     nix-bash-completions  # nix bash completions
 
     direnv  # directory environments
-    
+
     exa  # replacement for ls
-    
+
     bat  # replacement for cat with syntax highlighting
-    
+
     pandoc  # convert documents into multiple formats
-    
+
     ripgrep  # alternative to rep
-    
+
     fd  # alternative to find
-    
+
     fselect  # search for files using SQL like queries
-    
+
     vifm  # text mode file manager
-    
+
     tmux  # terminal multiplexer
-    
+
     fzf  # fuzzy finder
-    
+
     tig  # terminal ui for git
-    
+
     lazygit  # alternative terminal ui for git
 
     git  # git version control
 
     jq jid  # JSON utilities
-    
+
     ncdu  # display disk usage
-    
+
     xsv  # csv utilities: https://github.com/BurntSushi/xsv/releases/latest
-    
+
     wuzz  # HTTP utilities
-    
+
     unzip  # unzip files
-    
+
     tree  # directories as a tree
-    
+
     minio minio-client  # S3 compatible server and client
 
     chezmoi  # manage user configurations (dotfiles) across machines
@@ -58,6 +58,10 @@ let
     sqlite  # sqlite command line interface
 
     vim  # terminal vim - latest version
-    
+
     neovim  # neovim editor
+
+    rclone  # rsync for the cloud (like S3)
+  ];
+
 in all
